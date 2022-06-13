7fdec1bd6122e8f1220db0e266e6affe2d2b66a5
deep-rune <theprune@protonmail.com>
2022-06-13 19:38:08 +0100

    feat: new Prune Handler for handling The Prune

--- helloworld.py
+++ helloworld.py
@@ -9,9 +9,14 @@ class HelloWorld(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello World, fellow pythonistas!")
 
+class PruneHandleHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Don't Request The Prune")
+
 app = tornado.web.Application([
     (r"/",IndexHandler),
-    (r"/helloworld",HelloWorld)
+    (r"/helloworld",HelloWorld),
+    (r"/prunehandle",PruneHandleHandler)
 ])
 app.listen(8080)
 tornado.ioloop.IOLoop.current().start()
